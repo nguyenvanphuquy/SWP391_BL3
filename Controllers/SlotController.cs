fd43c6bebb4055a12cf103590acd9427023bd98d
Phu Quy <quynso12@gmail.com>
2025-12-07 23:10:08 +0700

    SlotController

---
+++ Controllers/SlotController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SWP391_BL3.Repositories.Interfaces;
+
+namespace SWP391_BL3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlotController : ControllerBase
+    {
+        private readonly ISlotRepository _slotRepository;
+        public SlotController(ISlotRepository slotRepository)
+        {
+            _slotRepository = slotRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var slots = _slotRepository.GetAll();
+            return Ok(slots);
+        }
+    }
+}
